ac62667e01d6e2daa3ba1a43968d6b5b595cc741
Sherbul Mohammed <129470579+SherbulMohammed@users.noreply.github.com>
2024-02-04 22:01:00 +0000

    Add function getSize() to get user to pick dimensions of board.

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function(){
-   
-    console.log("hi")
+    createBoard(32);
+    
+
+    let btn_popup = document.querySelector("#pop-up");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
 })
 
-function createBoard() {
+function createBoard(size) {
     let board = document.querySelector(".board")
 
-    board.style.gridTemplateColumns = "repeat(16, 1fr)";
-    board.style.gridTemplateRows = "repeat(16, 1fr)";
-    
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement("div");
+        div.addEventListener("mouseover", colorDiv)
+        board.insertAdjacentElement("beforeend", div);
+    }
+
+}
+
+function getSize() {
+    let input = prompt("What dimensions will the board have?");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerText = "Please provide a number";
+    }
+    else if (input < 0 || input > 100){
+        message.innerText = "Please provide a number between 1 and 100"
+    }
+    else {
+        message.innerText = "Now you play!"
+        return input;
+    }
+}
+
+
+function colorDiv(){
+    if(colorChoice == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+}
+
+
+function setColor(color){
+    let color = colorChoice;
 }
